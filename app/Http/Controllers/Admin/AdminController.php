93394407ea489e098872f1e47cb46e97e0927ec6
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-06-17 16:33:53 -0500

    Update_Graphics

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -33,7 +33,7 @@ class AdminController extends Controller
         $categories = User::all();
         $users = User::all();
         $tags = Tag::all();
-        $comments = Comment::all();
+        $comments = Comment::latest('updated_at')->get();
 
         /*Variables de grafica de cantidad de comentarios por mes */
         $ene = DB::select('SELECT count(*) AS cantidad FROM comments WHERE created_at >= ? AND created_at < ?',['2020-01-01', '2020-01-31']);
@@ -50,6 +50,23 @@ class AdminController extends Controller
         $dic = DB::select('SELECT count(*) AS cantidad FROM comments WHERE created_at >= ? AND created_at < ?',['2020-12-01', '2020-12-31']);
         /*Fin de variables de grafica de cantidad de comentarios por mes */
 
-        return view('admin.dashboard', compact('posts', 'categories', 'users', 'tags', 'comments', 'ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'));
+        /*Variables de grafica de cantidad de usuarios registrados por mes */
+        $ene_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-01-01', '2020-01-31']);
+        $feb_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-02-01', '2020-02-29']);
+        $mar_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-03-01', '2020-03-31']);
+        $abr_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-04-01', '2020-04-30']);
+        $may_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-05-01', '2020-05-31']);
+        $jun_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-06-01', '2020-06-30']);
+        $jul_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-07-01', '2020-07-31']);
+        $ago_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-08-01', '2020-08-31']);
+        $sep_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-09-01', '2020-09-30']);
+        $oct_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-10-01', '2020-10-31']);
+        $nov_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-11-01', '2020-11-30']);
+        $dic_2 = DB::select('SELECT count(*) AS cantidad FROM users WHERE created_at >= ? AND created_at < ?',['2020-12-01', '2020-12-31']);
+        /*Fin de variables de grafica de cantidad de usuarios registrados por mes */
+
+        return view('admin.dashboard', compact('posts', 'categories', 'users', 'tags', 'comments'
+        , 'ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'
+        , 'ene_2', 'feb_2', 'mar_2', 'abr_2', 'may_2', 'jun_2', 'jul_2', 'ago_2', 'sep_2', 'oct_2', 'nov_2', 'dic_2'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -46,10 +46,128 @@
         </div>
     </div>
 
-
     <div class="row">
         <div class="col-8">
             <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script type="text/javascript">
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
+
+                function drawChart() {
+                    var data = google.visualization.arrayToDataTable([
+                        ['Mes', 'Cantidad'],
+                                    ['Ene', {{ $ene_2[0]->cantidad }} ],
+                                    ['Feb', {{ $feb_2[0]->cantidad }}],
+                                    ['Mar', {{ $mar_2[0]->cantidad }}],
+                                    ['Abr', {{ $abr_2[0]->cantidad }}],
+                                    ['May', {{ $may_2[0]->cantidad }}],
+                                    ['Jun', {{ $jun_2[0]->cantidad }}],
+                                    ['Jul', {{ $jul_2[0]->cantidad }}],
+                                    ['Ago', {{ $ago_2[0]->cantidad }}],
+                                    ['Sep', {{ $sep_2[0]->cantidad }}],
+                                    ['Oct', {{ $oct_2[0]->cantidad }}],
+                                    ['Nov', {{ $nov_2[0]->cantidad }}],
+                                    ['Dic', {{ $dic_2[0]->cantidad }}]
+                    ]);
+
+                    var options = {
+                    curveType: 'function',
+                    legend: { position: 'right' }
+                    };
+
+                    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+                    chart.draw(data, options);
+                }
+            </script>
+
+                <div class="card shadow p-3 mb-5 bg-white rounded border-top border-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="far fa-chart-bar"></i>
+                            Usuarios Registrados
+                        </h3>
+                    </div>
+                    <div class="card-body">
+                        <div id="curve_chart" style="width: 100%; height: 300px"></div>
+
+                    </div>
+                    <!-- /.card-body-->
+                </div>
+        </div>
+
+        <div class="col-4">
+        <div class="card shadow p-3 mb-5 bg-white rounded border-top border-primary" style="height: 421px; overflow: scroll; overflow-x: hidden;" >
+            <div class="card-header">
+                <h3 class="card-title">
+                    <i class="far fa-chart-bar"></i>
+                        Ranking de Lugares</h3>
+            </div>
+              <!-- /.card-header -->
+            <div class="card-body p-0">
+                <ul class="products-list product-list-in-card pl-2 pr-2">
+                @foreach ($posts as $post)
+                    <li class="item">
+                        <div class="product-img ">
+                        <img src="/images/{{ $post->urlimg }}" alt="Product Image" class="img-size-50 rounded">
+                        </div>
+                    <div class="product-info">
+                        <a href="javascript:void(0)" class="product-title">{{ $post->title }}
+                            <span class="badge badge-warning float-right">{{ $post->point }} </span>
+                        </a>
+                        <span class="product-description">
+                            {{ $post->excerpt }}
+                        </span>
+                    </div>
+                    </li>
+                @endforeach
+                <!-- /.item -->
+                </ul>
+            </div>
+              <!-- /.card-body -->
+        </div>
+        </div>
+    </div>
+
+    <div class="row">
+    <div class="col-4">
+    <!-- DIRECT CHAT -->
+                <div class="card direct-chat direct-chat-primary border-top border-primary shadow rounded">
+                  <div class="card-header">
+                    <h3 class="card-title">Ultimos Comentarios</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                    <!-- Conversations are loaded here -->
+                    <div class="direct-chat-messages" style="height: 367px;">
+@foreach($comments as $comment)
+                      <!-- Message to the right -->
+                      <div class="direct-chat-msg ">
+                        <div class="direct-chat-infos clearfix">
+                          <span class="direct-chat-name float-right">{{ $comment->post->title }}</span>
+                          <span class="direct-chat-timestamp float-left">{{ $comment->user->name }}</span>
+                        </div>
+                        <!-- /.direct-chat-infos -->
+                        <img class="direct-chat-img" src="/images/{{ $comment->user->avatar }}" alt="message user image">
+                        <!-- /.direct-chat-img -->
+                        <div class="direct-chat-text">
+                        {{ $comment->text }}
+                        </div>
+                        <!-- /.direct-chat-text -->
+                      </div>
+                      <!-- /.direct-chat-msg -->
+                      @endforeach
+                    </div>
+                    <!--/.direct-chat-messages-->
+
+                    
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!--/.direct-chat -->
+        </div>
+
+        <div class="col-8">
             <script type="text/javascript">
                 google.charts.load('current', {
                     'packages': ['bar']
@@ -88,7 +206,7 @@
                 <div class="card-header">
                     <h3 class="card-title">
                         <i class="far fa-chart-bar"></i>
-                        Comentarios
+                        Comentarios Publicados
                     </h3>
                 </div>
                 <div class="card-body">
@@ -97,38 +215,9 @@
                 <!-- /.card-body-->
             </div>
         </div>
-
-        <div class="col-4">
-        <div class="card shadow p-3 mb-5 bg-white rounded border-top border-primary" style="height: 421px; overflow: scroll;" >
-              <div class="card-header">
-                <h3 class="card-title">
-                <i class="far fa-chart-bar"></i>
-                Ranking de Lugares</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body p-0">
-                <ul class="products-list product-list-in-card pl-2 pr-2">
-                @foreach ($posts as $post)
-                  <li class="item">
-                    <div class="product-img ">
-                      <img src="/images/{{ $post->urlimg }}" alt="Product Image" class="img-size-50 rounded">
-                    </div>
-                    <div class="product-info">
-                      <a href="javascript:void(0)" class="product-title">{{ $post->title }}
-                        <span class="badge badge-warning float-right">{{ $post->point }} </span></a>
-                      <span class="product-description">
-                      {{ $post->excerpt }}
-                      </span>
-                    </div>
-                  </li>
-                  @endforeach
-                  <!-- /.item -->
-                </ul>
-              </div>
-              <!-- /.card-body -->
-            </div>
-        </div>
     </div>
+
+    
 </section>
 
 
